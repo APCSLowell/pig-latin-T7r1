5590add58a4e6a5b296f0caa38456a6747de4d67
T7r1 <142773849+T7r1@users.noreply.github.com>
2024-01-17 12:39:33 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -5,6 +5,7 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class PigLatin {
     
     public void tester() {
+	    String [] vowels={"a","e","i","o","u"};
         // String[] lines = loadStrings("words.txt");
         String[] lines = new String[8]; 
         try{
